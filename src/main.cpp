6ff43b5c09c88b48913c0d3ea0b4263cd3bbb488
toxypiks <HannaHinkelbein89@gmx.de>
2025-02-19 14:40:53 +0100

    Implement simple script to read in data from command line and print it

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-    cout << "Hello Seaman!\n";
+    cout << "Please enter your first_name and age!\n";
+    string first_name;
+    double age;
+    cin >> first_name;
+    cin >> age;
+    cout << "Hello, " << first_name << " (age " << age*12 << " months)\n";
     return 0;
 }
